1ac660f14ad8b68394f6b80926d113bc62cdfc2f
ranahiren27 <ranahiren27@gmail.com>
2018-07-03 17:25:28 +0530

    adding new fetures

--- Lexical_analysis/teockes_code1_version2.cpp
+++ Lexical_analysis/teockes_code1_version2.cpp
@@ -23,9 +23,37 @@ int isKeyword(char buffer[]){
     
     return flag;
 }
- 
+
+bool isChar(char ch){
+
+  if(ch == 'a')
+    return true;
+  else
+    return false;
+
+}
+
+char isIdentifier(char buffer[]){
+  char ch,flag;
+
+    for(int i=0;i<strlen(buffer);i++){
+          if((buffer[0] == '0')||(buffer[0] == '1')||
+            (buffer[0] == '2')||(buffer[0] == '3')||
+            (buffer[0] == '4')||(buffer[0] == '5')||
+            (buffer[0] == '6')||(buffer[0] == '7')||
+            (buffer[0] == '8')||(buffer[0] == '9')){
+            flag = 'c';
+          }else if( i>0 && flag=='n' && isChar(buffer[i]) ){
+            return 'f';
+          }else{
+            flag = 'i';
+          }
+    }
+
+  return flag;
+}
 int main(){
-    char ch, buffer[15], operators[] = "+-*/%=";
+    char ch, buffer[15], operators[] = "+-*/%=()  ";
     ifstream fin("program.txt");
     int i,j=0;
     
@@ -35,24 +63,34 @@ int main(){
     }
     
     while(!fin.eof()){
-           ch = fin.get();
+
+        ch = fin.get();
            
-        for(i = 0; i < 6; ++i){
+        for(i = 0; i < 8; ++i){
                if(ch == operators[i])
                    cout<<ch<<" is operator\n";
            }
            
            if(isalnum(ch)){
                buffer[j++] = ch;
+           //    cout<<buffer<<"checking\n";
            }
+
            else if((ch == ' ' || ch == '\n') && (j != 0)){
                    buffer[j] = '\0';
                    j = 0;
                                       
-                   if(isKeyword(buffer) == 1)
+                   if(isKeyword(buffer) == 1){
                        cout<<buffer<<" is keyword\n";
-                   else
-                       cout<<buffer<<" is indentifier\n";
+                   }else{
+                       if(isIdentifier(buffer) == 'c'){
+                          cout<<buffer<<" is constant\n";
+                       }else if(isIdentifier(buffer) == 'i'){
+                          cout<<buffer<<" is identifier\n";
+                       }else{
+                          cout<<buffer<<" is unvalid tocken\n";
+                       }
+                   }
            }
            
     }
